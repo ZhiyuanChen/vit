afa8566ed1cfa7eb3b45717e61122e1bb50eb1a5
Zhiyuan Chen <this@zyc.ai>
2020-12-21 18:33:01 +0800

    fix bugs in logging

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import logging
+import logging.config
 import math
 
 import numpy as np
@@ -71,11 +72,51 @@ def init(args):
             os.makedirs(experiment, exist_ok=True)
             writer = SummaryWriter(log_dir=experiment)
         if args.log_dir:
-            logger = logging.setup_logger(logdir)
+            logger = setup_logger(args.logdir)
         if args.train:
             save_dir = os.path.join(experiment, args.save_dir)
             os.makedirs(save_dir, exist_ok=True)
     return experiment, logger, writer, save_dir
+
+
+def setup_logger(log_dir):
+    """Creates and returns a fancy logger."""
+    # Why is setting up proper logging so !@?#! ugly?
+    os.makedirs(log_dir, exist_ok=True)
+    logging.config.dictConfig({
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'standard': {
+                'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+            },
+        },
+        'handlers': {
+            'stderr': {
+                'level': 'INFO',
+                'formatter': 'standard',
+                'class': 'logging.StreamHandler',
+                'stream': 'ext://sys.stderr',
+            },
+            'logfile': {
+                'level': 'DEBUG',
+                'formatter': 'standard',
+                'class': 'logging.FileHandler',
+                'filename': os.path.join(log_dir, 'train.log'),
+                'mode': 'a',
+            }
+        },
+        'loggers': {
+            '': {
+                'handlers': ['stderr', 'logfile'],
+                'level': 'DEBUG',
+                'propagate': True
+            },
+        }
+    })
+    logger = logging.getLogger(__name__)
+    logger.flush = lambda: [h.flush() for h in logger.handlers]
+    return logger
 
 
 def resume(model, optimizer, args):
