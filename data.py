33fc186288b765dcf5014302c87664433d982c96
Zhiyuan Chen <chenzhiyuan1@tetras.ai>
2021-01-07 02:49:06 +0800

    update get size

--- data.py
+++ data.py
@@ -1,3 +1,4 @@
+import time
 import sys
 import os
 import random
@@ -148,8 +149,7 @@ class RASampler(torch.utils.data.Sampler):
 def fast_collate(batch, memory_format):
     imgs = [img[0] for img in batch]
     targets = torch.tensor([target[1] for target in batch], dtype=torch.int64)
-    w = imgs[0].size[0]
-    h = imgs[0].size[1]
+    w, h = imgs[0].size
     tensor = torch.zeros((len(imgs), 3, h, w), dtype=torch.uint8).contiguous(memory_format=memory_format)
     for i, img in enumerate(imgs):
         nump_array = np.array(img, dtype=np.uint8)
