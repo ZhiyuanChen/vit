1fa4490235066d78103be8b26da5276029e0665e
Zhiyuan Chen <this@zyc.ai>
2021-01-07 10:52:55 +0800

    reformat code

--- data.py
+++ data.py
@@ -20,6 +20,7 @@ import numpy as np
 from utils import catch
 
 import warnings
+
 warnings.filterwarnings("ignore", "(Possibly )?corrupt EXIF data", UserWarning)
 
 
@@ -58,8 +59,8 @@ class DataFetcher(object):
     def __init__(self, loader):
         self.iter = iter(loader)
         self.stream = torch.cuda.Stream()
-        self.mean = torch.tensor([0.485 * 255, 0.456 * 255, 0.406 * 255]).cuda().view(1,3,1,1)
-        self.std = torch.tensor([0.229 * 255, 0.224 * 255, 0.225 * 255]).cuda().view(1,3,1,1)
+        self.mean = torch.tensor([0.485 * 255, 0.456 * 255, 0.406 * 255]).cuda().view(1, 3, 1, 1)
+        self.std = torch.tensor([0.229 * 255, 0.224 * 255, 0.225 * 255]).cuda().view(1, 3, 1, 1)
         self.preload()
 
     def preload(self):
@@ -153,13 +154,15 @@ def fast_collate(batch, memory_format):
     tensor = torch.zeros((len(imgs), 3, h, w), dtype=torch.uint8).contiguous(memory_format=memory_format)
     for i, img in enumerate(imgs):
         nump_array = np.array(img, dtype=np.uint8)
-        if(nump_array.ndim < 3):
+        if nump_array.ndim < 3:
             nump_array = np.expand_dims(nump_array, axis=-1)
         nump_array = np.rollaxis(nump_array, 2)
         tensor[i] += torch.from_numpy(nump_array)
     return tensor, targets
 
-def load_data(path, transform, batch_size, num_workers, memory_format, shuffle=None, distributed=True, profile=-1, collate_fn=None):
+
+def load_data(path, transform, batch_size, num_workers, memory_format, shuffle=None, distributed=True, profile=-1,
+              collate_fn=None):
     dataset = ImageFolder(path, transform)
 
     sampler = torch.utils.data.distributed.DistributedSampler(dataset) if distributed else None
@@ -177,4 +180,3 @@ def load_data(path, transform, batch_size, num_workers, memory_format, shuffle=N
     )
 
     return dataset, sampler, loader
-
