56bcb4704e1d488a91d17a716a72f689cdb7008c
Zhiyuan Chen <chenzhiyuan1@tetras.ai>
2021-01-21 21:59:36 +0800

    move transformer-related operations to transformer.py

--- models/vision_transformer.py
+++ models/vision_transformer.py
@@ -3,100 +3,7 @@ import torch.nn as nn
 
 from functools import partial
 
-from .utils import DropModule
-
-
-class MLPBlock(nn.Module):
-    def __init__(self, in_channels, hidden_channels=None, out_channels=None,
-                 dropout=0.):
-        super().__init__()
-        hidden_channels = hidden_channels or in_channels
-        out_channels = out_channels or in_channels
-        self.fc1 = nn.Linear(in_channels, hidden_channels)
-        self.gelu = nn.GELU()
-        self.fc2 = nn.Linear(hidden_channels, out_channels)
-        self.dropout = nn.Dropout(dropout)
-
-    def forward(self, x):
-        x = self.fc1(x)
-        x = self.gelu(x)
-        x = self.dropout(x)
-        x = self.fc2(x)
-        x = self.dropout(x)
-        return x
-
-
-class Attention(nn.Module):
-    def __init__(self, dim, num_heads=16, attn_bias=True, attn_scaling=None,
-                 attn_dropout=0.):
-        super().__init__()
-        self.num_heads = num_heads
-        head_dim = dim // num_heads
-        self.scaling = attn_scaling or head_dim ** -0.5
-        self.in_proj = nn.Linear(dim, dim * 3, bias=attn_bias)
-        self.dropout = nn.Dropout(attn_dropout)
-        self.out_proj = nn.Linear(dim, dim)
-
-    def forward(self, x):
-        B, N, C = x.shape
-        q, k, v = self.in_proj(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
-        attention = (q @ k.transpose(-2, -1)) * self.scaling
-        attention = attention.softmax(dim=-1)
-        attention = self.dropout(attention)
-        x = (attention @ v).transpose(1, 2).reshape(B, N, C)
-        x = self.out_proj(x)
-        return x
-
-
-class Encoder1DBlock(nn.Module):
-    def __init__(self, hidden_size, num_heads, mlp_ratio=4, dropout=0.,
-                 attn_bias=True, attn_scaling=None, attn_dropout=0.,
-                 norm=nn.LayerNorm, drop_prob=0.):
-        super().__init__()
-        self.norm1 = norm(hidden_size)
-        self.attention = Attention(
-            hidden_size, num_heads=num_heads, attn_bias=attn_bias,
-            attn_scaling=attn_scaling, attn_dropout=attn_dropout)
-        self.norm2 = norm(hidden_size)
-        mlp_dim = int(hidden_size * mlp_ratio)
-        self.mlp = MLPBlock(in_channels=hidden_size, hidden_channels=mlp_dim, dropout=dropout)
-        self.drop_module = DropModule(drop_prob) if drop_prob > 0. else nn.Identity()
-        self.dropout = nn.Dropout(dropout)
-
-    def forward(self, x):
-        residual = x.clone()
-        x = self.norm1(x)
-        x = self.attention(x)
-        x = self.drop_module(x)
-        x = x + residual
-        y = self.norm2(x)
-        y = self.mlp(y)
-        y = self.drop_module(y)
-        return x + y
-
-
-class Encoder(nn.Module):
-    def __init__(self, img_size=384, patches=16, hidden_size=1024,
-                 num_layers=12, num_heads=12, mlp_ratio=4, dropout=0.,
-                 attn_bias=True, attn_scaling=None, attn_dropout=0.,
-                 norm=nn.LayerNorm, drop_prob=0.):
-        super().__init__()
-        drop_probs = [x.item() for x in torch.linspace(0, drop_prob, num_layers)]
-        self.blocks = nn.ModuleList(
-            [Encoder1DBlock(
-                hidden_size=hidden_size, num_heads=num_heads,
-                mlp_ratio=mlp_ratio, dropout=dropout, attn_bias=attn_bias,
-                attn_scaling=attn_scaling, attn_dropout=attn_dropout,
-                norm=norm, drop_prob=drop_probs[i]
-            ) for i in range(num_layers)]
-        )
-        self.norm = norm(hidden_size)
-
-    def forward(self, x):
-        for block in self.blocks:
-           x = block(x)
-        x = self.norm(x)
-        return x[:, 0]
+from .transformer import Encoder
 
 
 class VisionTransformer(nn.Module):
@@ -115,9 +22,9 @@ class VisionTransformer(nn.Module):
         self.pos_embed = nn.Parameter(
             torch.zeros(1, num_patches + 1, hidden_size))
         self.dropout = nn.Dropout(dropout)
-        self.encoder = Encoder(img_size, patches, hidden_size, num_layers,
-            num_heads, mlp_ratio, dropout, attn_bias, attn_scaling,
-            attn_dropout, norm)
+        self.encoder = Encoder(
+            hidden_size, num_layers, num_heads, mlp_ratio, dropout, attn_bias,
+            attn_scaling, attn_dropout, norm)
         if pre_size:
             pre_size = pre_size if type(pre_size) is int else hidden_size
         self.pre_logits = nn.Linear(hidden_size, pre_size) if pre_size else nn.Identity()
@@ -157,11 +64,13 @@ def s16(pretrained=False, **kwargs):
         **kwargs)
     return model
 
+
 def b16(pretrained=False, **kwargs):
     model = VisionTransformer(
         patches=16, hidden_size=768, num_layers=12,
         num_heads=12, norm=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     return model
+
 
 def b32(pretrained=False, **kwargs):
     model = VisionTransformer(
@@ -169,17 +78,20 @@ def b32(pretrained=False, **kwargs):
         num_heads=12, norm=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     return model
 
+
 def l16(pretrained=False, **kwargs):
     model = VisionTransformer(
         patches=16, hidden_size=1024, num_layers=24,
         num_heads=16, norm=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     return model
 
+
 def l32(pretrained=False, **kwargs):
     model = VisionTransformer(
         patches=32, hidden_size=1024, num_layers=24,
         num_heads=16, norm=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     return model
+
 
 def h14(pretrained=False, **kwargs):
     model = VisionTransformer(
