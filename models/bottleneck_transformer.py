4daa5213321fe9d9f9daeedeb92c3fd1268f8783
Zhiyuan Chen <this@zyc.ai>
2021-01-31 00:50:18 +0800

    TEMP switch to open source version

--- models/bottleneck_transformer.py
+++ models/bottleneck_transformer.py
@@ -1,190 +1,144 @@
 import torch
 import torch.nn as nn
-
-from torch import Tensor, einsum
-from einops import rearrange, repeat
-
-from typing import Type, Any, Callable, Union, List, Optional
-
-from .resnet import ResNet, Bottleneck, BasicBlock, conv1x1
+import torch.nn.functional as F
 
 
-def rel_to_abs(x):
-    b, h, l, _ = x.shape
-    col_pad = torch.zeros((b, h, l, 1), device=x.device, dtype=x.dtype)
-    x = torch.cat((x, col_pad), dim=3)
-    flat_x = rearrange(x, 'b h l c -> b h (l c)')
-    flat_pad = torch.zeros((b, h, l - 1), dtype=x.dtype)
-    flat_x_padded = torch.cat((flat_x, flat_pad), dim=2)
-    final_x = flat_x_padded.reshape(b, h, l + 1, 2 * l - 1)
-    final_x = final_x[:, :, :l, (l - 1):]
-    return final_x
+def get_n_params(model):
+    pp = 0
+    for p in list(model.parameters()):
+        nn = 1
+        for s in list(p.size()):
+            nn = nn * s
+        pp += nn
+    return pp
 
 
-def relative_logits_1d(q, rel_k):
-    b, heads, h, w, dim = q.shape
-    logits = einsum('b h x y d, r d -> b h x y r', q, rel_k) * dim ** -0.5
-    logits = rearrange(logits, 'b h x y r -> b (h x) y r')
-    logits = rel_to_abs(logits)
-    logits = logits.reshape(b, heads, h, w, w)
-    logits = repeat(logits, 'b h x y j -> b h x i y j', i=h)
-    return logits
+class MHSA(nn.Module):
+    def __init__(self, n_dims, width=14, height=14):
+        super(MHSA, self).__init__()
 
+        self.query = nn.Conv2d(n_dims, n_dims, kernel_size=1)
+        self.key = nn.Conv2d(n_dims, n_dims, kernel_size=1)
+        self.value = nn.Conv2d(n_dims, n_dims, kernel_size=1)
 
-class AbsPosEmb(nn.Module):
-    def __init__(
-        self,
-        fmap_size,
-        dim_head
-    ):
-        super().__init__()
-        scale = dim_head ** -0.5
-        self.scale = scale
-        self.height = nn.Parameter(torch.randn(fmap_size, dim_head) * scale)
-        self.width = nn.Parameter(torch.randn(fmap_size, dim_head) * scale)
-
-    def forward(self, q):
-        emb = rearrange(self.height, 'h d -> h () d') + rearrange(self.width, 'w d -> () w d')
-        emb = rearrange(emb, ' h w d -> (h w) d')
-        logits = einsum('b h i d, j d -> b h i j', q, emb) * self.scale
-        return logits
-
-
-class RelPosEmb(nn.Module):
-    def __init__(
-        self,
-        fmap_size,
-        dim_head
-    ):
-        super().__init__()
-        scale = dim_head ** -0.5
-        self.fmap_size = fmap_size
-        self.scale = scale
-        self.rel_height = nn.Parameter(torch.randn(fmap_size * 2 - 1, dim_head) * scale)
-        self.rel_width = nn.Parameter(torch.randn(fmap_size * 2 - 1, dim_head) * scale)
-
-    def forward(self, q):
-        q = rearrange(q, 'b h (x y) d -> b h x y d', x=self.fmap_size)
-        rel_logits_w = relative_logits_1d(q, self.rel_width)
-        rel_logits_w = rearrange(rel_logits_w, 'b h x i y j-> b h (x y) (i j)')
-
-        q = rearrange(q, 'b h x y d -> b h y x d')
-        rel_logits_h = relative_logits_1d(q, self.rel_height)
-        rel_logits_h = rearrange(rel_logits_h, 'b h x i y j -> b h (y x) (j i)')
-        return rel_logits_w + rel_logits_h
-
-
-class Attention(nn.Module):
-    def __init__(self, planes, num_heads=4, height=14, width=14, attn_bias=False, rel_pos_emb=False, **kwargs):
-        super(Attention, self).__init__()
-        self.num_heads = num_heads
-        dim_head = planes // num_heads
-        self.scale = torch.sqrt(torch.tensor(dim_head, dtype=torch.float))
-        self.in_proj = nn.Conv2d(planes, planes * 3, kernel_size=1, bias=attn_bias)
-
-        pos_emb = AbsPosEmb if not rel_pos_emb else RelPosEmb
-        self.pos_emb = pos_emb(53, dim_head)
+        self.rel_h = nn.Parameter(torch.randn([1, n_dims, 1, height]), requires_grad=True)
+        self.rel_w = nn.Parameter(torch.randn([1, n_dims, width, 1]), requires_grad=True)
 
         self.softmax = nn.Softmax(dim=-1)
 
     def forward(self, x):
-        N, (B, C, H, W) = self.num_heads, x.shape
-        q, k, v = self.in_proj(x).chunk(3, dim=1)
-        q, k, v = map(lambda t: rearrange(t, 'b (h d) x y -> b h (x y) d', h=N), (q, k, v))
-        sim = einsum('b h i d, b h j d -> b h i j', q, k) * self.scale
-        sim += self.pos_emb(q)
-        attn = sim.softmax(dim=-1, dtype=q.dtype)
-        out = einsum('b h i j, b h j d -> b h i d', attn, v)
-        out = rearrange(out, 'b h (x y) d -> b (h d) x y', x=H, y=W)
-        return out
+        n_batch, C, width, height = x.size()
+        q = self.query(x).view(n_batch, C, -1)
+        k = self.key(x).view(n_batch, C, -1)
+        v = self.value(x).view(n_batch, C, -1)
 
+        content_content = torch.bmm(q.permute(0, 2, 1), k)
 
-class MHSABlock(nn.Module):
-    expansion: int = 4
+        content_position = (self.rel_h + self.rel_w).view(1, C, -1).permute(0, 2, 1)
+        content_position = torch.matmul(content_position, q)
 
-    def __init__(
-        self,
-        inplanes: int,
-        planes: int,
-        height: int,
-        width: int,
-        downsample: Optional[nn.Module] = None,
-        base_width: int = 64,
-        attn_bias: bool = False,
-        norm: Optional[Callable[..., nn.Module]] = nn.BatchNorm2d
-    ) -> None:
-        super(MHSABlock, self).__init__()
-        planes = int(planes * (base_width / 64.))
-        self.conv1 = conv1x1(inplanes, planes)
-        self.bn1 = norm(planes)
-        self.attention = Attention(planes, height, width, attn_bias)
-        self.bn2 = norm(planes)
-        self.conv3 = conv1x1(planes, planes * self.expansion)
-        self.bn3 = norm(planes * self.expansion)
-        self.relu = nn.ReLU(inplace=True)
-        self.downsample = downsample
+        energy = content_content + content_position
+        attention = self.softmax(energy)
 
-    def forward(self, x: Tensor) -> Tensor:
-        identity = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.attention(out)
-        out = self.bn2(out)
-        out = self.relu(out)
-
-        out = self.conv3(out)
-        out = self.bn3(out)
-
-        if self.downsample is not None:
-            identity = self.downsample(x)
-
-        out += identity
-        out = self.relu(out)
+        out = torch.bmm(v, attention.permute(0, 2, 1))
+        out = out.view(n_batch, C, width, height)
 
         return out
 
 
-class BoTNet(ResNet):
+class Bottleneck(nn.Module):
+    expansion = 4
 
-    def __init__(
-        self,
-        block: Type[Union[BasicBlock, Bottleneck, MHSABlock]],
-        layers: List[int],
-        num_classes: int = 1000,
-        zero_init_residual: bool = False,
-        groups: int = 1,
-        width_per_group: int = 64,
-        replace_stride_with_dilation: Optional[List[bool]] = None,
-        norm: Optional[Callable[..., nn.Module]] = nn.BatchNorm2d
-    ) -> None:
-        super(BoTNet, self).__init__(block=block, layers=layers, num_classes=num_classes,
-                                     zero_init_residual=zero_init_residual, groups=groups,
-                                     width_per_group=width_per_group,
-                                     replace_stride_with_dilation=replace_stride_with_dilation, norm=norm)
-        self.layer4 = self._make_mhsa(layers[3], 512)
+    def __init__(self, in_planes, planes, stride=1, mhsa=False, resolution=None):
+        super(Bottleneck, self).__init__()
 
-    def _make_mhsa(self, blocks: int, planes: int, stride: int = 1,
-                   height: int = 14, width: int = 14) -> nn.Sequential:
-        norm = self._norm
-        downsample = None
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        if not mhsa:
+            self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, padding=1, stride=stride, bias=False)
+        else:
+            self.conv2 = nn.ModuleList()
+            self.conv2.append(MHSA(planes, width=int(resolution[0]), height=int(resolution[1])))
+            if stride == 2:
+                self.conv2.append(nn.AvgPool2d(2, 2))
+            self.conv2 = nn.Sequential(*self.conv2)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, self.expansion * planes, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(self.expansion * planes)
 
-        if stride != 1:
-            downsample = nn.Sequential(
-                conv1x1(self.inplanes, planes * MHSABlock.expansion, stride),
-                norm(planes * MHSABlock.expansion),
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion * planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride),
+                nn.BatchNorm2d(self.expansion * planes)
             )
 
-        layer0 = MHSABlock(
-            self.inplanes, planes, height=height, width=width, norm=norm,
-            base_width=self.base_width, downsample=downsample)
-        height //= stride
-        width //= stride
-        layers = [MHSABlock(
-            self.inplanes, planes, height=height, width=width, norm=norm,
-            base_width=self.base_width)
-            for _ in range(1, blocks)]
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
 
-        return nn.Sequential(layer0, *layers)
+
+# reference
+# https://github.com/kuangliu/pytorch-cifar/blob/master/models/resnet.py
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=1000, resolution=(224, 224)):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+        self.resolution = list(resolution)
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        # self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        if self.conv1.stride[0] == 2:
+            self.resolution[0] /= 2
+        if self.conv1.stride[1] == 2:
+            self.resolution[1] /= 2
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)  # for ImageNet
+        if self.maxpool.stride == 2:
+            self.resolution[0] /= 2
+            self.resolution[1] /= 2
+
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2, mhsa=True)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Sequential(
+            nn.Dropout(0.3),  # All architecture deeper than ResNet-200 dropout_rate: 0.2
+            nn.Linear(512 * block.expansion, num_classes)
+        )
+
+    def _make_layer(self, block, planes, num_blocks, stride=1, mhsa=False):
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for idx, stride in enumerate(strides):
+            layers.append(block(self.in_planes, planes, stride, mhsa, self.resolution))
+            if stride == 2:
+                self.resolution[0] /= 2
+                self.resolution[1] /= 2
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.relu(self.bn1(self.conv1(x)))
+        out = self.maxpool(out)  # for ImageNet
+
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+
+        out = self.avgpool(out)
+        out = torch.flatten(out, 1)
+        out = self.fc(out)
+        return out
+
+
+def ResNet50(num_classes=1000, resolution=(256, 256)):
+    return ResNet(Bottleneck, [3, 4, 6, 3], num_classes=num_classes, resolution=resolution)
