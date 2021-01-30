063ca3124defebbe4aab5303b8799cf74773f9c0
Zhiyuan Chen <this@zyc.ai>
2021-01-31 00:05:50 +0800

    redo arch

--- models/bottleneck_transformer.py
+++ models/bottleneck_transformer.py
@@ -1,41 +1,105 @@
 import torch
-from torch import Tensor
 import torch.nn as nn
+
+from torch import Tensor, einsum
+from einops import rearrange, repeat
+
 from typing import Type, Any, Callable, Union, List, Optional
 
-from .resnet import conv1x1
+from .resnet import ResNet, Bottleneck, BasicBlock, conv1x1
+
+
+def rel_to_abs(x):
+    b, h, l, _ = x.shape
+    col_pad = torch.zeros((b, h, l, 1), device=x.device, dtype=x.dtype)
+    x = torch.cat((x, col_pad), dim=3)
+    flat_x = rearrange(x, 'b h l c -> b h (l c)')
+    flat_pad = torch.zeros((b, h, l - 1), dtype=x.dtype)
+    flat_x_padded = torch.cat((flat_x, flat_pad), dim=2)
+    final_x = flat_x_padded.reshape(b, h, l + 1, 2 * l - 1)
+    final_x = final_x[:, :, :l, (l - 1):]
+    return final_x
+
+
+def relative_logits_1d(q, rel_k):
+    b, heads, h, w, dim = q.shape
+    logits = einsum('b h x y d, r d -> b h x y r', q, rel_k) * dim ** -0.5
+    logits = rearrange(logits, 'b h x y r -> b (h x) y r')
+    logits = rel_to_abs(logits)
+    logits = logits.reshape(b, heads, h, w, w)
+    logits = repeat(logits, 'b h x y j -> b h x i y j', i=h)
+    return logits
+
+
+class AbsPosEmb(nn.Module):
+    def __init__(
+        self,
+        fmap_size,
+        dim_head
+    ):
+        super().__init__()
+        scale = dim_head ** -0.5
+        self.scale = scale
+        self.height = nn.Parameter(torch.randn(fmap_size, dim_head) * scale)
+        self.width = nn.Parameter(torch.randn(fmap_size, dim_head) * scale)
+
+    def forward(self, q):
+        emb = rearrange(self.height, 'h d -> h () d') + rearrange(self.width, 'w d -> () w d')
+        emb = rearrange(emb, ' h w d -> (h w) d')
+        logits = einsum('b h i d, j d -> b h i j', q, emb) * self.scale
+        return logits
+
+
+class RelPosEmb(nn.Module):
+    def __init__(
+        self,
+        fmap_size,
+        dim_head
+    ):
+        super().__init__()
+        scale = dim_head ** -0.5
+        self.fmap_size = fmap_size
+        self.scale = scale
+        self.rel_height = nn.Parameter(torch.randn(fmap_size * 2 - 1, dim_head) * scale)
+        self.rel_width = nn.Parameter(torch.randn(fmap_size * 2 - 1, dim_head) * scale)
+
+    def forward(self, q):
+        q = rearrange(q, 'b h (x y) d -> b h x y d', x=self.fmap_size)
+        rel_logits_w = relative_logits_1d(q, self.rel_width)
+        rel_logits_w = rearrange(rel_logits_w, 'b h x i y j-> b h (x y) (i j)')
+
+        q = rearrange(q, 'b h x y d -> b h y x d')
+        rel_logits_h = relative_logits_1d(q, self.rel_height)
+        rel_logits_h = rearrange(rel_logits_h, 'b h x i y j -> b h (y x) (j i)')
+        return rel_logits_w + rel_logits_h
 
 
 class Attention(nn.Module):
-    def __init__(self, planes, height=14, width=14, attn_bias=False):
+    def __init__(self, planes, num_heads=4, height=14, width=14, attn_bias=False, rel_pos_emb=False, **kwargs):
         super(Attention, self).__init__()
-
+        self.num_heads = num_heads
+        dim_head = planes // num_heads
+        self.scale = torch.sqrt(torch.tensor(dim_head, dtype=torch.float))
         self.in_proj = nn.Conv2d(planes, planes * 3, kernel_size=1, bias=attn_bias)
 
-        self.rel_h = nn.Parameter(torch.randn([1, planes, height, 1]))
-        self.rel_w = nn.Parameter(torch.randn([1, planes, 1, width]))
+        pos_emb = AbsPosEmb if not rel_pos_emb else RelPosEmb
+        self.pos_emb = pos_emb(53, dim_head)
 
         self.softmax = nn.Softmax(dim=-1)
 
     def forward(self, x):
-        b, c, h, w = x.shape
-        q, k, v = self.in_proj(x).chunk(3, dim=1).view(b, c, -1)
-
-        content_content = torch.bmm(q.permute(0, 2, 1), k)
-        content_position = (self.rel_h + self.rel_w).view(1, c, -1).permute(0, 2, 1)
-        content_position = torch.matmul(content_position, q)
-
-        energy = content_content + content_position
-        attention = self.softmax(energy)
-
-        out = torch.bmm(v, attention.permute(0, 2, 1))
-        out = out.view(b, c, h, w)
-
+        N, (B, C, H, W) = self.num_heads, x.shape
+        q, k, v = self.in_proj(x).chunk(3, dim=1)
+        q, k, v = map(lambda t: rearrange(t, 'b (h d) x y -> b h (x y) d', h=N), (q, k, v))
+        sim = einsum('b h i d, b h j d -> b h i j', q, k) * self.scale
+        sim += self.pos_emb(q)
+        attn = sim.softmax(dim=-1, dtype=q.dtype)
+        out = einsum('b h i j, b h j d -> b h i d', attn, v)
+        out = rearrange(out, 'b h (x y) d -> b (h d) x y', x=H, y=W)
         return out
 
 
 class MHSABlock(nn.Module):
-
     expansion: int = 4
 
     def __init__(
@@ -83,9 +147,26 @@ class MHSABlock(nn.Module):
         return out
 
 
-class BoTNet(nn.Module):
+class BoTNet(ResNet):
 
-    def _make_mhsa(self, planes: int, blocks: int = 3, stride: int = 1,
+    def __init__(
+        self,
+        block: Type[Union[BasicBlock, Bottleneck, MHSABlock]],
+        layers: List[int],
+        num_classes: int = 1000,
+        zero_init_residual: bool = False,
+        groups: int = 1,
+        width_per_group: int = 64,
+        replace_stride_with_dilation: Optional[List[bool]] = None,
+        norm: Optional[Callable[..., nn.Module]] = nn.BatchNorm2d
+    ) -> None:
+        super(BoTNet, self).__init__(block=block, layers=layers, num_classes=num_classes,
+                                     zero_init_residual=zero_init_residual, groups=groups,
+                                     width_per_group=width_per_group,
+                                     replace_stride_with_dilation=replace_stride_with_dilation, norm=norm)
+        self.layer4 = self._make_mhsa(layers[3], 512)
+
+    def _make_mhsa(self, blocks: int, planes: int, stride: int = 1,
                    height: int = 14, width: int = 14) -> nn.Sequential:
         norm = self._norm
         downsample = None
